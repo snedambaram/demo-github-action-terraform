dda38da3aa7235462e90dd0ef7e83fbfc1ebde49
unknown <nksreenivasan@gmail.com>
2024-09-09 18:43:07 -0400

    added provider1

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     key    = "github-actions-terraform-demo.tfstate"
     region = "us-east-2"
     profile = "sreeniprofile"
-    access_key = "AKIAV3Y3FSDDGRLK5U5B"
-    secret_key = "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"
+    access_key = "${{ secrets.AWS_DEPLOY_ACCESS_KEY_ID }}"
+    secret_key = "${{ secrets.AWS_DEPLOY_SECRET }}"
   }
 }
